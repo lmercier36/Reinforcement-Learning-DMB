238aa2cad1ac764d5fe0d801e95febc1661cb13e
lmercier36 <72507453+lmercier36@users.noreply.github.com>
2021-02-28 23:19:39 +0100

    Add files via upload

---
+++ pit.py
@@ -0,0 +1,53 @@
+import Arena
+from MCTS import MCTS
+from othello.OthelloGame import OthelloGame
+from othello.OthelloPlayers import *
+from othello.pytorch.NNet import NNetWrapper as NNet
+
+
+import numpy as np
+from utils import *
+
+"""
+use this script to play any two agents against each other, or play manually with
+any agent.
+"""
+
+mini_othello = True  # Play in 6x6 instead of the normal 8x8.
+human_vs_cpu = False
+
+if mini_othello:
+    g = OthelloGame(6)
+else:
+    g = OthelloGame(8)
+
+# all players
+rp = RandomPlayer(g).play
+gp = GreedyOthelloPlayer(g).play
+hp = HumanOthelloPlayer(g).play
+
+
+
+# nnet players
+n1 = NNet(g)
+if mini_othello:
+    n1.load_checkpoint('./Résultats/resNormal','checkpoint_19.pth.tar')
+else:
+    n1.load_checkpoint('./pretrained_models/othello/pytorch/','8x8_100checkpoints_best.pth.tar')
+args1 = dotdict({'numMCTSSims': 12, 'cpuct':1.0})
+mcts1 = MCTS(g, n1, args1)
+n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+if human_vs_cpu:
+    player2 = hp
+else:
+    n2 = NNet(g)
+    n2.load_checkpoint('./pretrained_models/othello/pytorch/', '6x6_153checkpoints_best.pth.tar')
+    args2 = dotdict({'numMCTSSims': 12, 'cpuct': 1.0})
+    mcts2 = MCTS(g, n2, args2)
+    n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
+    player2 = n2p
+
+arena = Arena.Arena(n1p, player2, g, display=OthelloGame.display)
+
+print(arena.playGames(50, verbose=False))
